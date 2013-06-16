ee7e69aa7ffc6dc7efaecebe15079d7f362f0283
Amy <sabreuse@gmail.com>
2013-06-16 13:54:24 -0400

    update wp readme with licensing info

--- cc-accessible-dropdown-menus.php
+++ cc-accessible-dropdown-menus.php
@@ -1,9 +1,28 @@
 <?php
 /*
 Plugin Name:  Keyboard Accessible Dropdown Menus
-Description:  Makes dropdown menus in default WordPress themes keyboard accessible
-Version:      0.3
-Author:       Amy Hendrix, based on previous work by Graham Armfield
+Description:  Makes dropdown menus in WordPress themes keyboard accessible
+Version:      0.4
+Author:       Amy Hendrix, Graham Armfield
+License:      GPL2
+Plugin URI:   http://github.com/sabreuse/accessible-menus
+Author URI:   http://amyhendrix.net
+*/
+
+/*  Copyright 2013 Amy Hendrix (email : amy@amyhendrix.net), Graham Armfield (email : graham.armfield@coolfields.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 function ccadm_scripts() {
