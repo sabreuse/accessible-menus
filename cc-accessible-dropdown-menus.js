1a9167fa2c7ce4494ab20f75fa7c7bd008600ebf
Amy <sabreuse@gmail.com>
2013-06-19 00:08:49 -0400

    Remove mobile check from JS now that we're testing before load.

--- cc-accessible-dropdown-menus.js
+++ cc-accessible-dropdown-menus.js
@@ -1,16 +1,9 @@
 (function($) {
-
-	function is_touch_device() {
-		return !!('ontouchstart' in window) || !!('onmsgesturechange' in window); // works on ie10
-	}
-
-	if (!is_touch_device()) {
-		$('[role=navigation] li').hover(
-			function(){$(this).addClass("ccadm-hover");},
-			function(){$(this).delay('250').removeClass("ccadm-hover");}
-		);
-		$('[role=navigation] li a').on('focus blur',
-			function(){$(this).parents().toggleClass("ccadm-hover");}
-		);
-	}
+	$('[role=navigation] li').hover(
+		function(){$(this).addClass("ccadm-hover");},
+		function(){$(this).delay('250').removeClass("ccadm-hover");}
+	);
+	$('[role=navigation] li a').on('focus blur',
+		function(){$(this).parents().toggleClass("ccadm-hover");}
+	);
 }(jQuery));
